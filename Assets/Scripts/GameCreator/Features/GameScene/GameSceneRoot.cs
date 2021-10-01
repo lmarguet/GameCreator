bc9b87cfe6cf482785ad6a9d9a3ba1d51b1c8467
Lucas Marguet <lucas.marguet@wooga.net>
2021-10-01 23:06:27 +0200

    Add raycasting for terrain

--- Assets/Scripts/GameCreator/Features/GameScene/GameSceneRoot.cs
+++ Assets/Scripts/GameCreator/Features/GameScene/GameSceneRoot.cs
@@ -5,11 +5,17 @@ namespace GameCreator.Features.GameScene
 {
     public class GameSceneRoot : ASceneRoot
     {
+        [SerializeField] Camera sceneCamera;
+        [SerializeField] LayerMask terrainLayer;
+        
         string selectedCharacter;
+        bool isMousePressed;
+        bool isTerrainPressed;
 
         void Start()
         {
             Debug.Log("[GameSceneRoot] Start");
+            isMousePressed = false;
         }
 
         public void SetSelectedCharacter(string characterId)
@@ -22,6 +28,50 @@ namespace GameCreator.Features.GameScene
         {
             selectedCharacter = null;
             Debug.Log("deselect");
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isMousePressed = true;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                isMousePressed = false;
+            }
+
+            if (isMousePressed)
+            {
+                var ray = sceneCamera.ScreenPointToRay (Input.mousePosition);
+
+                if (Physics.Raycast (ray, out var hit))
+                {
+                    var layerMask = 1 << hit.transform.gameObject.layer;
+                    if (layerMask == terrainLayer.value)
+                    {
+                        OnTerrainPress(hit.point);
+                    }
+                }   
+            }
+            else
+            {
+                isTerrainPressed = false;
+            }
+        }
+
+        void OnTerrainPress(Vector3 hitPoint)
+        {
+            if (!isTerrainPressed)
+            {
+                OnTerrainMouseDown(hitPoint);
+            }
+            isTerrainPressed = true;
+        }
+
+        void OnTerrainMouseDown(Vector3 hitPoint)
+        {
+            Debug.Log("Terrain mouse down");
         }
     }
 }
---
+++ Assets/Scripts/GameCreator/Features/GameScene/GameSceneTerrain.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GameCreator.Features.GameScene
+{
+    public class GameSceneTerrain : MonoBehaviour
+    {
+    }
+}
